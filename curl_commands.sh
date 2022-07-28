09cfbc5d39e9fa7a340e55d1e560680f4c34c913
davisaints <davi.santos4357@gmail.com>
2022-07-28 09:17:12 -0300

    General

---
+++ curl_commands.sh
@@ -0,0 +1,37 @@
+#To invoke JSONWS:
+curl http://localhost:8080/api/jsonws/ohqiwtsfhl.h7g5folder/add-my-custom-h7-g5-folder \
+  -d description="This is where I store all my vacation photos." \
+  -d name="Vacation Photos" \
+  -u test@liferay.com:test
+
+curl http://localhost:8080/api/jsonws/ohqiwtsfhl.h7g5folder/add-my-custom-h7-g5-folder \
+  -d description="Davi test" \
+  -d name="Davi" \
+  -u test@liferay.com:test
+
+# To invoke addMyCustomH7G5FolderWithPermissionCheck:
+  curl http://localhost:8080/api/jsonws/ohqiwtsfhl.h7g5folder/add-my-custom-h7-g5-folder-with-permission-check \
+  -d description="Invoke Permission Check" \
+  -d name="Permission Check" \
+  -u test@liferay.com:test 
+
+# I added a new user to Liferay and got the exception message "You are not test@liferay.com"
+  curl http://localhost:8080/api/jsonws/ohqiwtsfhl.h7g5folder/add-my-custom-h7-g5-folder-with-permission-check \
+  -d description="Using other user" \
+  -d name="Other user" \
+  -u davi@liferay.com:test 
+
+  #I used this curl command to invoke JSONWS and didn't invok
+  curl http://localhost:8080/api/jsonws/ohqiwtsfhl.h7g5entry/add-my-custom-h7-g5-entry-service-with-permission-check \
+  -d description="photo 2." \
+  -d name="desc 2" \
+  -u davi@liferay.com:test
+
+
+
+
+
+
+
+  
+
---
+++ h7g5-service/src/main/java/com/liferay/h7g5/service/impl/H7G5EntryServiceImpl.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.h7g5.service.impl;
+
+import com.liferay.h7g5.service.base.H7G5EntryServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.model.User;
+import java.util.Objects;
+import com.liferay.h7g5.model.H7G5Entry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = {
+		"json.web.service.context.name=ohqiwtsfhl",
+		"json.web.service.context.path=H7G5Entry"
+	},
+	service = AopService.class
+)
+public class H7G5EntryServiceImpl extends H7G5EntryServiceBaseImpl {
+	public H7G5Entry addMyCustomH7G5EntryServiceWithPermissionCheck(
+		String description, String name)
+		throws PortalException {
+        
+			checkEmailAdress();
+		
+			System.out.println(
+			 "Invoking H7G5EntryServiceImpl#addMyCustomH7G5Entry(" + description + ", " + name + ")");
+		
+			H7G5Entry h7g5Entry = h7g5EntryLocalService.createH7G5Entry(
+			 System.currentTimeMillis());
+		
+			h7g5Entry.setDescription(description);
+			h7g5Entry.setName(name);
+		
+			h7g5EntryLocalService.addH7G5Entry(h7g5Entry);
+		
+		
+			return h7g5Entry;
+		}
+
+		public int getH7G5EntriesCount(){
+			return h7g5EntryLocalService.getH7G5EntriesCount();
+		}
+
+	public void checkEmailAdress() throws PortalException {
+
+		User user = getUser();
+		
+
+		if (!Objects.equals(user.getEmailAddress(), "test@liferay.com")) {
+			throw new PrincipalException("You are not test@liferay.com");
+		}
+	}
+}
